5a4cdda0e4e118530f3756562f321179aa9dfb93
AaniketSadekar <aaniket0009@gmail.com>
2026-02-27 11:52:10 +0530

    Data Types

---
+++ fullStack/JS/dataTypes.js
@@ -0,0 +1,73 @@
+// Data Types
+
+// check dataTypes
+let no = 18382;
+console.log(typeof(no));
+
+
+// Primitive Data Type
+
+// Numberic Type
+
+// 1. Number
+let n = 12828399;
+console.log(n);
+
+let n2 = 3.14159;
+console.log(n2);
+
+// 2. bigint
+let n3 = 123456789012345678901234567890n;
+console.log(n2);
+
+// Non- Numberic Type
+
+// 1. String
+let name = "coder";
+console.log(name);
+
+// 2. Boolean
+let flag = true;
+console.log(flag);
+
+// 3. Null
+let nul = null;
+console.log(nul);
+
+// 4. undefined
+let n4;
+console.log(n4);
+
+// 5. symbol
+let sym = Symbol('unique');
+console.log(sym);
+
+
+// Non-Primitive DataTypes
+
+// 1.object
+let obj = {
+    name: "Amit",
+    age : 24
+};
+console.log(obj);
+
+// 2. Array
+let a = ["red","green","blue"];
+console.log(a);
+
+// 3. function
+function fun(){
+    console.log("JavaScript");
+}
+fun();
+
+// 
+
+
+
+
+
+
+
+
