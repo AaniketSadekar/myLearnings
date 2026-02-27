0be67874bb235c783c26bd98104854ff3911bcc7
AaniketSadekar <aaniket0009@gmail.com>
2026-02-27 11:53:43 +0530

    Operators

---
+++ fullStack/JS/Operators.js
@@ -0,0 +1,95 @@
+// Operators
+
+// Arithmetic Operators
+console.log("====== Arithmetic Operators ======");
+console.log(8+5);
+console.log(8-5);
+console.log(8*5);
+console.log(8/5);
+console.log(8%5);
+
+// Assignment Operators
+console.log("====== Assignment Operators ======");
+let n = 5;
+n +=5;
+console.log(n);
+
+n -=5;
+console.log(n);
+
+n *= 5;
+console.log(n);
+
+n /= 5;
+console.log(n);
+
+n %= 2;
+console.log(n);
+
+
+// Relational Operators
+console.log("====== Relational Operators ======");
+console.log(4<5);
+console.log(5<=5);
+console.log(6>5);
+console.log(5>=5);
+// in
+// instanceof
+
+
+
+// Logical Operators
+console.log("====== Logical Operators ======");
+
+// &&
+console.log(4>3 && 4<5);
+
+// ||
+console.log(4<5 || 4>3);
+
+// !
+console.log(!(4>5));
+
+
+// Comparison Operators
+console.log("====== Comparison Operators ======");
+console.log(4 == 4);
+console.log(4 != 5);
+console.log(4 === "Hi");
+console.log(4 !== "Hi");
+
+// Bitwise Operators
+console.log(5 & 1);
+console.log(5 | 1);
+console.log(5 ^ 1);
+console.log(~ 1);
+
+
+
+
+// comma Operators
+
+
+// Unary Operators
+console.log("====== Unary Operators ======");
+let x = 1;
+x++;
+console.log(x);
+
+x--;
+console.log(x);
+
+++x;
+console.log(x);
+
+--x;
+console.log(x);
+
+
+// Ternary Operators
+console.log("====== Ternary Operators ======");
+const age = 19;
+age>18? console.log("Adult") : console.log("Not Adult");
+
+
+
