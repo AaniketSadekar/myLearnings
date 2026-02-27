03ad2e7c867a70fac5c7adf5abe9f46801a98603
AaniketSadekar <aaniket0009@gmail.com>
2026-02-27 11:52:43 +0530

    Type Conversion

---
+++ fullStack/JS/typeConversion.js
@@ -0,0 +1,13 @@
+// Type conversion
+
+// Implicit Conversion => Automatically do conversion
+let n1 = 8; 
+let n2 = 4.5;
+console.log(n1+n2);
+
+
+// Explicit Conversion => Manually do convesion
+let sum = Number(n1+n2);
+console.log(sum);
+
+
