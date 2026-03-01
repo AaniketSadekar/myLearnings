4b91f87f6fdc60ac97973ffff00f99921bb21015
AaniketSadekar <aaniket0009@gmail.com>
2026-03-01 20:41:29 +0530

    Operators

---
+++ Python/Operators.py
@@ -0,0 +1,60 @@
+# Operators
+
+# Arithmetic Operators
+print("====== Arithmetic Operators ======")
+print(8+5)
+print(8-5)
+print(8*5)
+print(8/5)
+print(8%5)
+
+print()
+
+# Relational Operators
+print("====== Relational Operators ======")
+print(5==5)
+print(5!=4)
+print(5<6)
+print(5<=5)
+print(6>5)
+print(6>=5)
+
+print()
+
+# Logical Operators
+print("====== Logical Operators ======")
+print(5 > 4 and 6 > 5)
+print(4 > 3 or 4 > 5)
+print(not(5>6))
+
+print()
+
+# Bitwise Operator
+print("====== Bitwise Operators ======")
+print(5 & 4)
+print(5 | 4)
+print(5 ^ 4)
+print(~ 5)
+print(5<<2)
+print(5>>2)
+
+print()
+
+print("====== Assignment Operators ======")
+n = 5
+print(n)
+
+n += 5
+print(n)
+
+n -= 5
+print(n)
+
+n *= 5
+print(n)
+
+n /= 2
+print(n)
+
+n %= 2
+print(n)
