0207919e5ebff4588c5e82572368d41d44d285b4
AaniketSadekar <aaniket0009@gmail.com>
2026-03-01 11:55:44 +0530

    Data Types

---
+++ Python/dataTypes.py
@@ -0,0 +1,70 @@
+# data Types
+
+# Numberic 
+print("====== Numberic Data Type ======")
+
+# 1. int 
+n = 1
+print(type(1),n)
+
+# 2. float 
+n1 = 3.14
+print(type(n1),n1)
+
+# 3. complex 
+n2 = 2 + 6j
+print(type(n2),n2)
+
+print()
+
+# Sequence Data Type
+print("====== Sequence Data Type ======")
+
+# 1. string
+s = 's'
+print(type(s),s)
+
+s1 = "ss"
+print(type(s1), s1)
+
+s2 = ''' 
+string
+data 
+type
+'''
+print(type(s2),s2)
+
+# 2. list => mutable 
+l = [1,2,3,4,5]
+print(type(l), l)
+
+print()
+
+# 3. tuple => Immutable
+tup = (1,2,3,4,5)
+print(type(tup), tup)
+
+print()
+
+# Boolean
+print("====== Boolean Data Type ======")
+is_flag = True
+print(type(is_flag), is_flag)
+
+print()
+
+# Dictionary
+print("====== Dictionary Data Type ======")
+d = {1:'One',2:'Two',3:'Three'}
+print(type(d),d)
+
+print()
+
+# Set
+print("====== Set Data Type ======")
+st = set('abc')
+print(type(st),st)
+
+
+
+
