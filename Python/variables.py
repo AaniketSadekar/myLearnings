3d9a0477352309e2e6c8bed0bf32148c825c4dc8
AaniketSadekar <aaniket0009@gmail.com>
2026-03-01 11:09:39 +0530

    Variables

---
+++ Python/variables.py
@@ -0,0 +1,17 @@
+# variables
+
+
+# valid variable
+first_name = "John";
+First_name = "John";
+first_Name = "John";
+First_Name = "John";
+roll_no_1 = 1;
+
+
+# Invalid Variable
+'''
+    first name = "John";
+    1 roll no = 1;
+
+'''
